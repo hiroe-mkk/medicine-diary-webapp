5e4ae580808486e07388356e595532f572ce85fa
hiroe-kk <sntarmi78@gmail.com>
2023-09-20 15:36:55 +0900

    chore: サブネットを追加

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -6,3 +6,20 @@ variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 }
+
+variable "azs" {
+  type = map(object({
+    public_cidr  = string
+    private_cidr = string
+  }))
+  default = {
+    a = {
+      public_cidr  = "10.0.1.0/24"
+      private_cidr = "10.0.11.0/24"
+    },
+    c = {
+      public_cidr  = "10.0.2.0/24"
+      private_cidr = "10.0.12.0/24"
+    }
+  }
+}
