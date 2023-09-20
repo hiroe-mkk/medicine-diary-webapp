5d059017917f730fd63fa43ce1c4cad2b5b806e4
hiroe-kk <sntarmi78@gmail.com>
2023-09-20 16:13:02 +0900

    chore: セキュリティグループを追加

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -3,3 +3,10 @@ module "network" {
 
   prefix = local.prefix
 }
+
+module "security_group" {
+  source = "../../modules/security_group"
+
+  prefix = local.prefix
+  vpc_id = module.network.aws_vpc_this_id
+}
---
+++ terraform/modules/security_group/main.tf
@@ -0,0 +1,73 @@
+# ---------------------------
+# VPC 外部からの HTTP と HTTPS の通信を許可するセキュリティグループ
+# ---------------------------
+resource "aws_security_group" "web" {
+  name   = "${var.prefix}-web-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.prefix}-web-sg"
+  }
+}
+
+
+# ---------------------------
+# VPC 内部リソース同士のあらゆる通信を許可するセキュリティグループ
+# ---------------------------
+resource "aws_security_group" "vpc" {
+  name   = "${var.prefix}-vpc-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.prefix}-vpc-sg"
+  }
+}
