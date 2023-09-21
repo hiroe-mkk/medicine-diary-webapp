b696c1674af79e22ca87bda70d21431e9e792635
hiroe-kk <sntarmi78@gmail.com>
2023-09-21 17:07:40 +0900

    chore: frontendサブモジュールにVue.js関連のパッケージをインストール

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,9 +1,13 @@
 const Webpack = require('webpack');
 const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-cheap-module-source-map',
+  entry: {
+    vendor: ['vue'],
+  },
   output: {
     // ビルド成果物はルートプロジェクトの src/main/resources/static/dist に出力する
     path: path.resolve(__dirname, '../src/main/resources/static/dist'),
@@ -11,6 +15,14 @@ module.exports = {
     // ビルド成果物を出力する前に、出力ディレクトリをクリーンアップする
     clean: true,
   },
+  plugins: [
+    new Webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true, // オプションAPIサポートの有効化
+      __VUE_PROD_DEVTOOLS__: false, //　本番環境での開発ツールのサポートを無効化
+    }),
+    // .vue ファイルをビルドしてバンドルする
+    new VueLoaderPlugin(),
+  ],
   module: {
     rules: [
       {
@@ -20,12 +32,23 @@ module.exports = {
         // node_modules を対象外にする
         exclude: /node_modules/,
       },
+      {
+        // .vue ファイルをビルドしてバンドルする
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        // .vue ファイルの style をバンドルして style 要素として埋め込む
+        test: /\.vue\.css$/,
+        use: ['vue-style-loader', 'css-loader'],
+      },
     ],
   },
   resolve: {
     // モジュールを解決する際は node_modules から検索する
     modules: ['node_modules'],
     alias: {
+      vue$: 'vue/dist/vue.esm-bundler.js',
       '@main': path.resolve(__dirname, 'src/main/'),
     },
   },
