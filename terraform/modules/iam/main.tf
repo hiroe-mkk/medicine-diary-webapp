9fa1456c599081a333a43318e19b26d1a00ca625
hiroe-kk <sntarmi78@gmail.com>
2023-09-22 20:42:40 +0900

    chore: タスク実行ロールにSSMから値を取得するために必要なポリシーをアタッチ

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -24,11 +24,36 @@ resource "aws_iam_role" "ecs_task_execution" {
   }
 }
 
-data "aws_iam_policy" "ecs_task_execution" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_iam_policy" "ssm" {
+  name = "${var.prefix}-ssm"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameters",
+            "ssm:GetParameter",
+            "kms:Decrypt"
+          ],
+          "Resource" : "arn:aws:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.self.account_id}:parameter/${var.prefix}/*"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${var.prefix}-ssm"
+  }
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = data.aws_iam_policy.ecs_task_execution.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ssm.arn
 }
