d11e403c493fcdfd7705dbf912da8e61e4ea925a
hiroe-kk <sntarmi78@gmail.com>
2023-09-27 09:39:25 +0900

    test: ControllerTestがトランザクション管理されるように変更

--- src/test/kotlin/example/testhelper/springframework/autoconfigure/ControllerTest.kt
+++ src/test/kotlin/example/testhelper/springframework/autoconfigure/ControllerTest.kt
@@ -2,11 +2,13 @@ package example.testhelper.springframework.autoconfigure
 
 import org.springframework.boot.test.autoconfigure.web.servlet.*
 import org.springframework.boot.test.context.*
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * Controller クラスをテストする場合に利用可能なアノテーション
  */
 @SpringBootTest
 @EnableTestDataInserter
+@Transactional
 @AutoConfigureMockMvc
 annotation class ControllerTest
