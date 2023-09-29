2423fc984316b308fd22fe0caaae75ecb916b0ac
hiroe-kk <sntarmi78@gmail.com>
2023-09-29 13:48:48 +0900

    chore: DBサブネットグループを追加

--- terraform/modules/network/outputs.tf
+++ terraform/modules/network/outputs.tf
@@ -5,3 +5,7 @@ output "aws_vpc_this_id" {
 output "aws_subnet_public_ids" {
   value = [for s in aws_subnet.public : s.id]
 }
+
+output "aws_db_subnet_group_this_id" {
+  value = aws_db_subnet_group.this.id
+}
