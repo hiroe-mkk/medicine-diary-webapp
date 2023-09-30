fc92edf6569fd9d29f841e4b199d6fb1c8aef0b9
hiroe-kk <sntarmi78@gmail.com>
2023-09-30 12:22:22 +0900

    chore: セキュリティポリシーを変更

--- terraform/modules/routing/alb.tf
+++ terraform/modules/routing/alb.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.this.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
 
   default_action {
     type = "forward"
