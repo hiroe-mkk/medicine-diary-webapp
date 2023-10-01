96d8fa8846750e383c4b71e5ed72231d0be63743
hiroe-kk <sntarmi78@gmail.com>
2023-10-01 16:36:46 +0900

    refactor: ユーザー名をprivateに変更

--- src/main/kotlin/example/application/service/profile/UsernameChangeCommand.kt
+++ src/main/kotlin/example/application/service/profile/UsernameChangeCommand.kt
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.*
 
 data class UsernameChangeCommand(@field:Size(max = 30,
                                              message = "{max}文字以内で入力してください。")
-                                 val username: String) {
+                                 private val username: String) {
     val validatedUsername: Username = Username(username.trim())
 }
