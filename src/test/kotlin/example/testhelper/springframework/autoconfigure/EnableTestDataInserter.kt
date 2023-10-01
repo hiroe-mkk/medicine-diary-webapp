08c34115a1f4885b4ce250a5bf80d83ba7a2689e
hiroe-kk <sntarmi78@gmail.com>
2023-10-01 12:04:48 +0900

    docs: クラスの概要を説明するドキュメントを追加

--- src/test/kotlin/example/testhelper/springframework/autoconfigure/EnableTestDataInserter.kt
+++ src/test/kotlin/example/testhelper/springframework/autoconfigure/EnableTestDataInserter.kt
@@ -6,6 +6,9 @@ import example.testhelper.inserter.*
 import org.springframework.context.annotation.*
 import org.springframework.stereotype.*
 
+/**
+ * モックされた TestDataInserter の AutoConfiguration を有効にするアノテーション
+ */
 @Import(EnableTestDataInserter.Configuration::class)
 annotation class EnableTestDataInserter {
     class Configuration(private val accountRepository: AccountRepository,
