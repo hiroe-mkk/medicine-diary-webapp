fe2eda6d17277a7894e6b7865b995c04fa4fa366
hiroe-kk <sntarmi78@gmail.com>
2023-10-01 21:30:27 +0900

    refactor: フォームを送信する処理をメソッドに抽出

--- frontend/src/main/js/components/profile/ProfileEdit.vue
+++ frontend/src/main/js/components/profile/ProfileEdit.vue
@@ -198,28 +198,11 @@ function submitUsernameChangeForm() {
   const form = new URLSearchParams();
   form.set('username', editingUsername.value);
   form.set('_csrf', props.csrf);
-
-  HttpRequestClient.submitPostRequest('/api/profile/username/change', form)
-    .then(() => {
-      profile.username = editingUsername.value;
-      isUsernameChangeModalActive.value = false;
-      resultMessage.value.activate('INFO', 'ユーザー名の変更が完了しました。');
-    })
-    .catch((error) => {
-      if (error instanceof HTTPRequestFailedError) {
-        if (error.status == 400) {
-          // バインドエラーが発生した場合
-          if (!error.isBodyEmpty() && error.body.fieldErrors !== undefined) {
-            fieldErrors.set(error.body.fieldErrors);
-            return;
-          }
-        } else if (error.status == 401) {
-          // 認証エラーが発生した場合
-          location.reload();
-          return;
-        }
-      }
-    });
+  submitForm('/api/profile/username/change', form, () => {
+    profile.username = editingUsername.value;
+    isUsernameChangeModalActive.value = false;
+    resultMessage.value.activate('INFO', 'ユーザー名の変更が完了しました。');
+  });
 }
 
 function activateProfileImageChangeModal() {
@@ -241,15 +224,20 @@ async function submitProfileImageChangeForm() {
   const form = new FormData();
   form.set('profileImage', result);
   form.set('_csrf', props.csrf);
+  submitForm('/api/profile/profileimage/change', form, () => {
+    profile.profileImage = URL.createObjectURL(result);
+    isProfileImageChangeModalActive.value = false;
+    resultMessage.value.activate(
+      'INFO',
+      'プロフィール画像の変更が完了しました。'
+    );
+  });
+}
 
-  HttpRequestClient.submitPostRequest('/api/profile/profileimage/change', form)
+function submitForm(url, form, successHandler) {
+  HttpRequestClient.submitPostRequest(url, form)
     .then(() => {
-      profile.profileImage = URL.createObjectURL(result);
-      isProfileImageChangeModalActive.value = false;
-      resultMessage.value.activate(
-        'INFO',
-        'プロフィール画像の変更が完了しました。'
-      );
+      successHandler();
     })
     .catch((error) => {
       if (error instanceof HTTPRequestFailedError) {
