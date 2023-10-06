31b895c8f4b5380cdbe405f2f6df22bba251ffb1
hiroe-kk <sntarmi78@gmail.com>
2023-10-06 19:53:45 +0900

    add: 薬詳細画面に薬詳細を追加

--- src/main/kotlin/example/domain/shared/type/Note.kt
+++ src/main/kotlin/example/domain/shared/type/Note.kt
@@ -1,5 +1,13 @@
 package example.domain.shared.type
 
 data class Note(val value: String) {
+    fun isBlank(): Boolean {
+        return value.isBlank()
+    }
+
+    fun toLines(): List<String> {
+        return value.split("\n")
+    }
+
     override fun toString() = value
 }
