5d83dfa73e815e9794f5b4e1dad78c872acc7cc1
hiroe-kk <sntarmi78@gmail.com>
2023-10-06 09:56:53 +0900

    update: 小数点以下が0のみの場合は、小数点以下を表示しないように変更

--- src/main/kotlin/example/domain/model/medicine/Dosage.kt
+++ src/main/kotlin/example/domain/model/medicine/Dosage.kt
@@ -4,5 +4,7 @@ package example.domain.model.medicine
  * 用量 (1回あたりの服薬量)
  */
 data class Dosage(val quantity: Double) {
-    override fun toString(): String = quantity.toString()
+    override fun toString(): String {
+        return Regex(".0+\$").replace(quantity.toString(), "")
+    }
 }
