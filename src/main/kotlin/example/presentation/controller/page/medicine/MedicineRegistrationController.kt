6dc641e129b04ebec7908f9a417c119c31a44b71
hiroe-kk <sntarmi78@gmail.com>
2023-10-06 20:11:03 +0900

    change: 薬登録リクエストのレスポンスボディとしてhtmlを返すように変更

--- src/main/kotlin/example/presentation/controller/page/medicine/MedicineRegistrationController.kt
+++ src/main/kotlin/example/presentation/controller/page/medicine/MedicineRegistrationController.kt
@@ -2,23 +2,50 @@ package example.presentation.controller.page.medicine
 
 import example.application.service.medicine.*
 import example.domain.model.medicine.*
+import example.domain.shared.message.*
 import example.presentation.shared.usersession.*
 import org.springframework.stereotype.*
 import org.springframework.ui.*
+import org.springframework.validation.*
+import org.springframework.validation.annotation.*
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.*
 
 @Controller
 @RequestMapping("/medicines/register")
-class MedicineRegistrationController {
+class MedicineRegistrationController(private val medicineService: MedicineService,
+                                     private val userSessionProvider: UserSessionProvider) {
+    @ModelAttribute("timings")
+    fun timings(): Array<Timing> = Timing.values()
+
+    @ModelAttribute("title")
+    fun title(): String = "お薬登録"
+
+    @ModelAttribute("executePath")
+    fun executePath(): String = "/medicines/register"
+
     /**
      * 薬登録画面を表示する
      */
     @GetMapping
     fun displayMedicineRegistrationPage(model: Model): String {
         model.addAttribute("form", MedicineBasicInfoInputCommand.initialize())
-        model.addAttribute("title", "お薬登録")
-        model.addAttribute("timings", Timing.values())
-        model.addAttribute("executePath", "/medicines/register")
         return "medicine/form"
+    }
+
+    /**
+     * 薬を登録する
+     */
+    @PostMapping
+    fun registerMedicine(@ModelAttribute("form") @Validated medicineBasicInfoInputCommand: MedicineBasicInfoInputCommand,
+                         bindingResult: BindingResult,
+                         redirectAttributes: RedirectAttributes): String {
+        if (bindingResult.hasErrors()) return "medicine/form"
+
+        medicineService.registerMedicine(medicineBasicInfoInputCommand,
+                                         userSessionProvider.getUserSession())
+        redirectAttributes.addFlashAttribute("resultMessage",
+                                             ResultMessage.info("薬の登録が完了しました。"))
+        return "redirect:/medicines"
     }
 }
--- src/test/kotlin/example/presentation/controller/page/medicine/MedicineRegistrationControllerTest.kt
+++ src/test/kotlin/example/presentation/controller/page/medicine/MedicineRegistrationControllerTest.kt
@@ -1,9 +1,12 @@
 package example.presentation.controller.page.medicine
 
+import example.domain.model.medicine.*
 import example.testhelper.springframework.autoconfigure.*
 import example.testhelper.springframework.security.*
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.*
+import org.springframework.security.test.web.servlet.request.*
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
 import org.springframework.test.web.servlet.*
 import org.springframework.test.web.servlet.request.*
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
@@ -16,26 +19,107 @@ internal class MedicineRegistrationControllerTest(@Autowired private val mockMvc
         private const val PATH = "/medicines/register"
     }
 
-    @Test
-    @WithMockAuthenticatedAccount
-    @DisplayName("薬登録画面を表示する")
-    fun displayMedicineRegistrationPage() {
-        //when:
-        val actions = mockMvc.perform(get(PATH))
+    @Nested
+    inner class DisplayMedicineRegistrationPageTest {
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("薬登録画面を表示する")
+        fun displayMedicineRegistrationPage() {
+            //when:
+            val actions = mockMvc.perform(get(PATH))
 
-        //then:
-        actions.andExpect(status().isOk)
-            .andExpect(view().name("medicine/form"))
+            //then:
+            actions.andExpect(status().isOk)
+                .andExpect(view().name("medicine/form"))
+        }
+
+        @Test
+        @DisplayName("未認証ユーザによるリクエストの場合、トップページ画面へリダイレクトする")
+        fun requestedByUnauthenticatedUser_redirectsToToppagePage() {
+            //when:
+            val actions = mockMvc.perform(get(PATH))
+
+            //then:
+            actions.andExpect(status().isFound)
+                .andExpect(redirectedUrl("/"))
+        }
     }
 
-    @Test
-    @DisplayName("未認証ユーザによるリクエストの場合、トップページ画面へリダイレクトする")
-    fun requestedByUnauthenticatedUser_redirectsToToppagePage() {
-        //when:
-        val actions = mockMvc.perform(get(PATH))
+    @Nested
+    inner class RegisterMedicineTest {
+        private val name = "ロキソニンS"
+        private val takingUnit = "錠"
+        private val quantity = 1.0
+        private val timesPerDay = 3
+        private val timingOptions = arrayOf(Timing.AS_NEEDED)
+        private val effects = arrayOf("頭痛", "解熱", "肩こり")
+        private val precautions = "服用間隔は4時間以上開けること。"
 
-        //then:
-        actions.andExpect(status().isFound)
-            .andExpect(redirectedUrl("/"))
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("薬の登録に成功した場合、薬概要一覧画面にリダイレクトする")
+        fun medicineRegistrationSucceeds_redirectToMedicineOverviewsPage() {
+            //when:
+            val actions = mockMvc.perform(post(PATH)
+                                              .with(csrf())
+                                              .param("name", name)
+                                              .param("quantity", quantity.toString())
+                                              .param("takingUnit", takingUnit)
+                                              .param("timesPerDay", timesPerDay.toString())
+                                              .param("timingOptions", timingOptions[0].name)
+                                              .param("effects", effects[0])
+                                              .param("effects", effects[1])
+                                              .param("effects", effects[2])
+                                              .param("precautions", precautions))
+
+            //then:
+            actions.andExpect(status().isFound)
+                .andExpect(redirectedUrl("/medicines"))
+        }
+
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("バリデーションエラーが発生した場合、薬登録画面を再表示する")
+        fun validationErrorOccurs_redisplayMedicineRegistrationPage() {
+            //given:
+            val invalidName = ""
+
+            //when:
+            val actions = mockMvc.perform(post(PATH)
+                                              .with(csrf())
+                                              .param("name", invalidName)
+                                              .param("quantity", quantity.toString())
+                                              .param("takingUnit", takingUnit)
+                                              .param("timesPerDay", timesPerDay.toString())
+                                              .param("timingOptions", timingOptions[0].name)
+                                              .param("effects", effects[0])
+                                              .param("effects", effects[1])
+                                              .param("effects", effects[2])
+                                              .param("precautions", precautions))
+
+            //then:
+            actions.andExpect(status().isOk)
+                .andExpect(view().name("medicine/form"))
+        }
+
+        @Test
+        @DisplayName("未認証ユーザによるリクエストの場合、トップページ画面にリダイレクトする")
+        fun requestedByUnauthenticatedUser_redirectToToppagePage() {
+            //when:
+            val actions = mockMvc.perform(post(PATH)
+                                              .with(csrf())
+                                              .param("name", name)
+                                              .param("quantity", quantity.toString())
+                                              .param("takingUnit", takingUnit)
+                                              .param("timesPerDay", timesPerDay.toString())
+                                              .param("timingOptions", timingOptions[0].name)
+                                              .param("effects", effects[0])
+                                              .param("effects", effects[1])
+                                              .param("effects", effects[2])
+                                              .param("precautions", precautions))
+
+            actions.andExpect(status().isFound)
+                .andExpect(redirectedUrl("/"))
+        }
     }
 }
