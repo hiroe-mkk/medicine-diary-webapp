088f9c106e858fb8f6fc83097df6d631cbdf005e
hiroe-kk <sntarmi78@gmail.com>
2023-10-07 16:09:50 +0900

    docs: ProfileImageURLクラスの概要を説明するドキュメントを修正

--- src/main/kotlin/example/domain/model/account/profile/profileimage/ProfileImageURL.kt
+++ src/main/kotlin/example/domain/model/account/profile/profileimage/ProfileImageURL.kt
@@ -3,7 +3,7 @@ package example.domain.model.account.profile.profileimage
 import example.domain.shared.type.*
 
 /**
- * プロフィール画像フルパス
+ * プロフィール画像URL
  */
 class ProfileImageURL(endpoint: String,
                       path: String) : URL(endpoint, path)
