775750a73258c3539f6cc69a05abb8e68e500851
hiroe-kk <sntarmi78@gmail.com>
2023-10-11 09:22:40 +0900

    add: 服用記録を追加するリクエストハンドラを追加

--- src/main/kotlin/example/presentation/controller/page/takingrecord/TakingRecordAdditionController.kt
+++ src/main/kotlin/example/presentation/controller/page/takingrecord/TakingRecordAdditionController.kt
@@ -3,14 +3,19 @@ package example.presentation.controller.page.takingrecord
 import example.application.service.medicine.*
 import example.application.service.takingrecord.*
 import example.domain.model.medicine.*
+import example.domain.shared.message.*
 import example.presentation.shared.usersession.*
 import org.springframework.stereotype.*
 import org.springframework.ui.*
+import org.springframework.validation.*
+import org.springframework.validation.annotation.*
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.*
 
 @Controller
 @RequestMapping("/takingrecords/add")
 class TakingRecordAdditionController(private val medicineService: MedicineService,
+                                     private val takingRecordService: TakingRecordService,
                                      private val userSessionProvider: UserSessionProvider) {
     @ModelAttribute("medicines")
     fun medicines(): Map<MedicineId, String> {
@@ -31,5 +36,21 @@ class TakingRecordAdditionController(private val medicineService: MedicineServic
     fun displayTakingRecordAdditionPage(model: Model): String {
         model.addAttribute("form", TakingRecordEditCommand.initialize())
         return "takingrecord/form"
+    }
+
+    /**
+     * 服用記録を追加する
+     */
+    @PostMapping
+    fun addTakingRecord(@ModelAttribute("form") @Validated takingRecordEditCommand: TakingRecordEditCommand,
+                        bindingResult: BindingResult,
+                        redirectAttributes: RedirectAttributes): String {
+        if (bindingResult.hasErrors()) return "takingrecord/form"
+
+        takingRecordService.addTakingRecord(takingRecordEditCommand,
+                                            userSessionProvider.getUserSession())
+        redirectAttributes.addFlashAttribute("resultMessage",
+                                             ResultMessage.info("服用記録の追加が完了しました。"))
+        return "redirect:/mypage" // TODO: 服用記録追加画面を表示していた画面に遷移するように変更する
     }
 }
--- src/test/kotlin/example/presentation/controller/page/takingrecord/TakingRecordAdditionControllerTest.kt
+++ src/test/kotlin/example/presentation/controller/page/takingrecord/TakingRecordAdditionControllerTest.kt
@@ -1,18 +1,30 @@
 package example.presentation.controller.page.takingrecord
 
+import example.application.service.takingrecord.*
+import example.application.service.takingrecord.TakingRecordEditCommand.*
+import example.domain.model.medicine.*
+import example.domain.model.takingrecord.*
 import example.presentation.controller.page.medicine.*
+import example.presentation.shared.usersession.*
+import example.testhelper.inserter.*
 import example.testhelper.springframework.autoconfigure.*
 import example.testhelper.springframework.security.*
+import org.bouncycastle.asn1.x500.style.RFC4519Style.*
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.*
+import org.springframework.security.test.web.servlet.request.*
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
 import org.springframework.test.web.servlet.*
 import org.springframework.test.web.servlet.request.*
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import java.time.*
 
 @ControllerTest
-internal class TakingRecordAdditionControllerTest(@Autowired private val mockMvc: MockMvc) {
+internal class TakingRecordAdditionControllerTest(@Autowired private val mockMvc: MockMvc,
+                                                  @Autowired private val testMedicineInserter: TestMedicineInserter,
+                                                  @Autowired private val userSessionProvider: UserSessionProvider) {
     companion object {
         private const val PATH = "/takingrecords/add"
     }
@@ -38,6 +50,80 @@ internal class TakingRecordAdditionControllerTest(@Autowired private val mockMvc
             val actions = mockMvc.perform(get(PATH))
 
             //then:
+            actions.andExpect(status().isFound)
+                .andExpect(redirectedUrl("/"))
+        }
+    }
+
+    @Nested
+    inner class AddTakingRecordTest {
+        private val quantity: Double? = 1.0
+        private val symptoms: List<FollowUpInputField> =
+                listOf(FollowUpInputField("頭痛", ConditionLevel.A_LITTLE_BAD, null))
+        private val note: String = ""
+        private val takenAt: String = "2000-01-01T07:00"
+
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("服用記録の追加に成功した場合、薬概要一覧画面にリダイレクトする")
+        fun takingRecordAdditionSucceeds_redirectToMyPage() {
+            //given:
+            val userSession = userSessionProvider.getUserSession()
+            val medicine = testMedicineInserter.insert(userSession.accountId)
+
+            //when:
+            val actions = mockMvc.perform(post(PATH)
+                                              .with(csrf())
+                                              .param("takenMedicine", medicine.id.value)
+                                              .param("quantity", quantity.toString())
+                                              .param("symptoms.get(0).symptom", symptoms[0].symptom)
+                                              .param("symptoms.get(0).beforeTaking", symptoms[0].beforeTaking?.name)
+                                              .param("note", note)
+                                              .param("takenAt", takenAt))
+
+            //then:
+            actions.andExpect(status().isFound)
+                .andExpect(redirectedUrl("/mypage"))
+        }
+
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("バリデーションエラーが発生した場合、薬登録画面を再表示する")
+        fun validationErrorOccurs_redisplayTakingRecordAdditionPage() {
+            //given:
+            val invalidMedicineId = ""
+
+            //when:
+            val actions = mockMvc.perform(post(PATH)
+                                              .with(csrf())
+                                              .param("takenMedicine", invalidMedicineId)
+                                              .param("quantity", quantity.toString())
+                                              .param("symptoms.get(0).symptom", symptoms[0].symptom)
+                                              .param("symptoms.get(0).beforeTaking", symptoms[0].beforeTaking?.name)
+                                              .param("note", note)
+                                              .param("takenAt", takenAt))
+
+            //then:
+            actions.andExpect(status().isOk)
+                .andExpect(view().name("takingrecord/form"))
+        }
+
+        @Test
+        @DisplayName("未認証ユーザによるリクエストの場合、トップページ画面にリダイレクトする")
+        fun requestedByUnauthenticatedUser_redirectToToppagePage() {
+            //given:
+            val medicineId = "medicineId"
+
+            //when:
+            val actions = mockMvc.perform(post(PATH)
+                                              .with(csrf())
+                                              .param("takenMedicine", medicineId)
+                                              .param("quantity", quantity.toString())
+                                              .param("symptoms.get(0).symptom", symptoms[0].symptom)
+                                              .param("symptoms.get(0).beforeTaking", symptoms[0].beforeTaking?.name)
+                                              .param("note", note)
+                                              .param("takenAt", takenAt))
+
             actions.andExpect(status().isFound)
                 .andExpect(redirectedUrl("/"))
         }
