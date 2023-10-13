c06fc1d7aae00d0452590fe02f4bf2486ddc5cc0
hiroe-kk <sntarmi78@gmail.com>
2023-10-13 18:17:47 +0900

    update: 認証に成功した場合に、クライアントによって要求された最後のパスに遷移するように変更

--- src/main/kotlin/example/presentation/shared/springframework/security/AuthenticationSuccessHandlerImpl.kt
+++ src/main/kotlin/example/presentation/shared/springframework/security/AuthenticationSuccessHandlerImpl.kt
@@ -3,8 +3,12 @@ package example.presentation.shared.springframework.security
 import example.domain.shared.message.*
 import example.presentation.shared.springframework.mvc.*
 import jakarta.servlet.http.*
+import org.springframework.http.server.*
 import org.springframework.security.core.*
 import org.springframework.security.web.authentication.*
+import org.springframework.security.web.savedrequest.*
+import org.springframework.web.util.pattern.*
+import java.net.*
 
 class AuthenticationSuccessHandlerImpl : AuthenticationSuccessHandler {
     /**
@@ -13,10 +17,27 @@ class AuthenticationSuccessHandlerImpl : AuthenticationSuccessHandler {
     override fun onAuthenticationSuccess(request: HttpServletRequest,
                                          response: HttpServletResponse,
                                          authentication: Authentication?) {
-        // TODO: 認証リクエスト以前にクライアントによって要求された最後のパスにリダイレクトするように修正する
         val redirectResolver = RedirectResolver(request, response)
         redirectResolver.addFlashAttribute("resultMessage",
                                            ResultMessage.info("ログインに成功しました。"))
-        redirectResolver.sendRedirect("/mypage")
+        val redirectPath = getRedirectPath(request, response)
+        redirectResolver.sendRedirect(redirectPath)
+    }
+
+    private fun getRedirectPath(request: HttpServletRequest, response: HttpServletResponse): String {
+        val lastRequestedURI = getPathOfLastRequestedPath(request, response)?.let { excludeErrorPagePaths(it) }
+        return lastRequestedURI ?: "/mypage"
+    }
+
+    private fun getPathOfLastRequestedPath(request: HttpServletRequest, response: HttpServletResponse): String? {
+        val savedRequest = HttpSessionRequestCache().getRequest(request, response)
+        val redirectUrl = savedRequest?.redirectUrl // クライアントによって要求された最後の URI
+        return redirectUrl?.let { URL(it).path } // URL のうちのパス部分のみを抽出する
+    }
+
+    private fun excludeErrorPagePaths(path: String): String? {
+        val pathPattern = PathPatternParser().parse("/error/**")
+        val pathContainer = PathContainer.parsePath(path)
+        return if (pathPattern.matches(pathContainer)) null else path
     }
 }
