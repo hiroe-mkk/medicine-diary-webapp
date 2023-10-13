de5c2dd68fec0ea7ee888dcacdea3e9f81078c49
hiroe-kk <sntarmi78@gmail.com>
2023-10-13 10:26:16 +0900

    delete: ProfileImageクラスを削除

--- src/main/kotlin/example/domain/model/account/profile/profileimage/ProfileImage.kt
+++
@@ -1,9 +0,0 @@
-package example.domain.model.account.profile.profileimage
-
-import example.domain.shared.type.*
-
-/**
- * プロフィール画像
- */
-class ProfileImage(val path: ProfileImageURL,
-                   val fileContent: FileContent)
