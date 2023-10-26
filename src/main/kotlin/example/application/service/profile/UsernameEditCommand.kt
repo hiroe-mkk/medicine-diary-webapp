ce52075f6eb3df5af25d158fb31ab22a357b2244
hiroe-kk <sntarmi78@gmail.com>
2023-10-26 21:38:26 +0900

    update: ユーザー名を入力必須項目に変更

--- src/main/kotlin/example/application/service/profile/UsernameEditCommand.kt
+++ src/main/kotlin/example/application/service/profile/UsernameEditCommand.kt
@@ -6,7 +6,8 @@ import jakarta.validation.constraints.*
 /**
  * ユーザー名の変更に利用される Command クラス
  */
-data class UsernameEditCommand(@field:Size(max = 30,
+data class UsernameEditCommand(@field:NotEmpty(message = "※ユーザー名を入力してください。")
+                               @field:Size(max = 30,
                                            message = "※{max}文字以内で入力してください。")
                                val username: String) {
     val validatedUsername: Username = Username(username.trim())
