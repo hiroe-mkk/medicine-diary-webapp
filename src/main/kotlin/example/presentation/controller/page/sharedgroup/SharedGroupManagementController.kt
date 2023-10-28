784850d4155da52af85bf43412652e62f3c59d25
hiroe-kk <sntarmi78@gmail.com>
2023-10-28 17:32:34 +0900

    add: 共有グループ管理画面を表示するリクエストハンドラを追加

---
+++ src/main/kotlin/example/presentation/controller/page/sharedgroup/SharedGroupManagementController.kt
@@ -0,0 +1,29 @@
+package example.presentation.controller.page.sharedgroup
+
+import example.application.query.sharedgroup.*
+import example.presentation.shared.session.*
+import example.presentation.shared.usersession.*
+import org.springframework.stereotype.*
+import org.springframework.ui.*
+import org.springframework.web.bind.annotation.*
+
+@Controller
+@RequestMapping("/sharedgroup")
+@SessionAttributes(types = [LastRequestedPagePath::class])
+class SharedGroupManagementController(private val sharedGroupQueryService: SharedGroupQueryService,
+                                      private val userSessionProvider: UserSessionProvider) {
+    @ModelAttribute("lastRequestedPagePath")
+    fun lastRequestedPagePath(): LastRequestedPagePath = LastRequestedPagePath("/sharedgroup")
+
+    /**
+     * 共有グループ管理画面を表示する
+     */
+    @GetMapping
+    fun displaySharedGroupManagementPage(model: Model): String {
+        val (participatingSharedGroup, invitedSharedGroups) =
+                sharedGroupQueryService.findSharedGroupDetails(userSessionProvider.getUserSession())
+        model.addAttribute("participatingSharedGroup", participatingSharedGroup)
+        model.addAttribute("invitedSharedGroups", invitedSharedGroups)
+        return "sharedgroup/management"
+    }
+}
