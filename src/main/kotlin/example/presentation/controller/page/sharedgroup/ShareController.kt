161610094ce44b991c26e5d336edd75740c99f98
hiroe-kk <sntarmi78@gmail.com>
2023-10-28 17:34:23 +0900

    add: 共有するリクエストハンドラを追加

---
+++ src/main/kotlin/example/presentation/controller/page/sharedgroup/ShareController.kt
@@ -0,0 +1,33 @@
+package example.presentation.controller.page.sharedgroup
+
+import example.application.service.sharedgroup.*
+import example.domain.model.account.*
+import example.domain.model.sharedgroup.*
+import example.domain.shared.message.*
+import example.presentation.shared.usersession.*
+import org.springframework.http.*
+import org.springframework.stereotype.*
+import org.springframework.validation.annotation.*
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.*
+
+@Controller
+@RequestMapping("/sharedgroup/share")
+class ShareController(private val sharedGroupService: SharedGroupService,
+                      private val userSessionProvider: UserSessionProvider) {
+    /**
+     * 共有する
+     */
+    @PostMapping
+    fun share(@ModelAttribute accountId: AccountId,
+              redirectAttributes: RedirectAttributes): String {
+        val resultMessage = try {
+            sharedGroupService.share(accountId, userSessionProvider.getUserSession())
+            ResultMessage.info("共有リクエストを送信しました。")
+        } catch (ex: ShareException) {
+            ex.resultMessage
+        }
+        redirectAttributes.addFlashAttribute("resultMessage", resultMessage)
+        return "redirect:/sharedgroup"
+    }
+}
---
+++ src/test/kotlin/example/presentation/controller/page/sharedgroup/ShareControllerTest.kt
@@ -0,0 +1,78 @@
+package example.presentation.controller.page.sharedgroup
+
+import example.domain.model.account.*
+import example.domain.model.medicine.*
+import example.presentation.shared.usersession.*
+import example.testhelper.inserter.*
+import example.testhelper.springframework.autoconfigure.*
+import example.testhelper.springframework.security.*
+import org.junit.jupiter.api.*
+import org.springframework.beans.factory.annotation.*
+import org.springframework.security.test.web.servlet.request.*
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
+import org.springframework.test.web.servlet.*
+import org.springframework.test.web.servlet.request.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@ControllerTest
+internal class ShareControllerTest(@Autowired private val mockMvc: MockMvc,
+                                   @Autowired private val testAccountInserter: TestAccountInserter,
+                                   @Autowired private val testSharedGroupInserter: TestSharedGroupInserter,
+                                   @Autowired private val userSessionProvider: UserSessionProvider) {
+    companion object {
+        private const val PATH = "/sharedgroup/share"
+    }
+
+    private lateinit var anotherAccountId: AccountId
+
+    @BeforeEach
+    internal fun setUp() {
+        anotherAccountId = testAccountInserter.insertAccountAndProfile().first.id
+    }
+
+    @Test
+    @WithMockAuthenticatedAccount
+    @DisplayName("共有に成功した場合、共有グループ管理画面にリダイレクトする")
+    fun shareSucceeds_redirectToShredGroupManagementPage() {
+        //when:
+        val actions = mockMvc.perform(post(PATH)
+                                          .with(csrf())
+                                          .param("accountId", anotherAccountId.value))
+
+        //then:
+        actions.andExpect(status().isFound)
+            .andExpect(redirectedUrl("/sharedgroup"))
+    }
+
+    @Test
+    @WithMockAuthenticatedAccount
+    @DisplayName("共有に失敗した場合、マイページ画面にリダイレクトする")
+    fun shareFails_displayNotFoundErrorPage() {
+        //given:
+        val userSession = userSessionProvider.getUserSession()
+        testSharedGroupInserter.insert(members = setOf(userSession.accountId))
+
+        //when:
+        val actions = mockMvc.perform(post(PATH)
+                                          .with(csrf())
+                                          .param("accountId", anotherAccountId.value))
+
+        //then:
+        actions.andExpect(status().isFound)
+            .andExpect(redirectedUrl("/mypage"))
+    }
+
+    @Test
+    @DisplayName("未認証ユーザによるリクエストの場合、トップページ画面にリダイレクトする")
+    fun requestedByUnauthenticatedUser_redirectToToppagePage() {
+        //when:
+        val actions = mockMvc.perform(post(PATH)
+                                          .with(csrf())
+                                          .param("accountId", anotherAccountId.value))
+
+        actions.andExpect(status().isFound)
+            .andExpect(redirectedUrl("/"))
+    }
+}
