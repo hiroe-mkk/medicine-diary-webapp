b2c089f9d2242b2c7022c2baff163a0d5a526595
hiroe-kk <sntarmi78@gmail.com>
2023-10-29 09:27:39 +0900

    add: 共有グループ管理画面に参加中の共有グループを追加

--- frontend/src/main/js/sharedGroupManagement.js
+++ frontend/src/main/js/sharedGroupManagement.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import UserSearch from '@main/js/components/user/UserSearch.vue';
+import noProfileImage from '@main/images/no_profile_image.png';
 
 createApp({
   data() {
     return {
       selectedUser: undefined,
+      noProfileImage: noProfileImage,
     };
   },
   methods: {
