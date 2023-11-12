d19169b9e62c83882305247f35215a10dff600fd
hiroe-kk <sntarmi78@gmail.com>
2023-11-12 17:11:51 +0900

    view: 本日の服用記録がnotificationの上に表示されるように変更

--- frontend/src/main/js/components/takingrecord/FilteredTakingRecordsByDate.vue
+++ frontend/src/main/js/components/takingrecord/FilteredTakingRecordsByDate.vue
@@ -1,91 +1,95 @@
 <template>
   <div class="container has-text-centered is-max-desktop p-3">
-    <div class="content">
-      <p class="icon-text is-size-4 is-flex is-justify-content-center">
-        <strong class="has-text-grey-dark">きょうの服用記録</strong>
-        <span class="icon has-text-grey-dark mx-2">
-          <i class="fa-solid fa-book-open"></i>
-        </span>
-      </p>
-    </div>
+    <div class="notification has-background-white-bis p-3">
+      <div class="content">
+        <p class="icon-text is-size-4 is-flex is-justify-content-center m-3">
+          <strong class="has-text-grey-dark">本日の服用記録</strong>
+          <span class="icon has-text-grey-dark mx-2">
+            <i class="fa-solid fa-book-open"></i>
+          </span>
+        </p>
+      </div>
 
-    <div class="content is-inline-block m-0">
-      <div class="is-flex is-align-items-center">
+      <div class="content is-inline-block m-0">
+        <div class="is-flex is-align-items-center">
+          <div
+            class="is-clickable mx-2"
+            :class="{ opacity: !filter.isMemberActive(member.accountId) }"
+            @click="toggleMemberActive(member.accountId)"
+            v-for="member in members"
+            v-if="members.length !== 1"
+          >
+            <div class="is-flex is-justify-content-center">
+              <figure class="image is-48x48 m-0">
+                <img
+                  class="is-rounded"
+                  :src="member.profileImageURL"
+                  v-if="member.profileImageURL !== undefined"
+                />
+                <img
+                  class="is-rounded"
+                  :src="noProfileImage"
+                  v-if="member.profileImageURL === undefined"
+                />
+              </figure>
+            </div>
+            <p
+              class="is-size-7 has-text-weight-bold has-text-grey-dark m-0 p-0"
+            >
+              {{ member.username }}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="content m-2" v-if="takingRecords.size !== 0" v-clock>
         <div
-          class="is-clickable mx-2"
-          :class="{ opacity: !filter.isMemberActive(member.accountId) }"
-          @click="toggleMemberActive(member.accountId)"
-          v-for="member in members"
-          v-if="members.length !== 1"
+          class="media is-flex is-align-items-center is-clickable p-3 m-0"
+          v-for="(takingRecord, takingRecordId) in takingRecords.values"
+          @click="activateTakingRecordModal(takingRecordId)"
         >
-          <div class="is-flex is-justify-content-center">
-            <figure class="image is-48x48 m-0">
+          <div class="media-left">
+            <figure class="image is-64x64 m-0">
               <img
+                :src="takingRecord.recorder.profileImageURL"
                 class="is-rounded"
-                :src="member.profileImageURL"
-                v-if="member.profileImageURL !== undefined"
+                v-if="takingRecord.recorder.profileImageURL !== undefined"
               />
               <img
-                class="is-rounded"
                 :src="noProfileImage"
-                v-if="member.profileImageURL === undefined"
+                class="is-rounded"
+                v-if="takingRecord.recorder.profileImageURL === undefined"
               />
             </figure>
           </div>
-          <p class="is-size-7 has-text-weight-bold has-text-grey-dark m-0 p-0">
-            {{ member.username }}
-          </p>
+          <div class="media-content has-text-left has-text-grey-dark">
+            <p class="m-0">
+              <span class="has-text-weight-bold">
+                {{ takingRecord.takenMedicine.medicineName }}
+              </span>
+            </p>
+            <p class="m-0">
+              <span>
+                {{ TakingRecordUtils.toTime(takingRecord.takenAt) }}
+              </span>
+            </p>
+          </div>
+          <div class="media-right">
+            <p class="icon fas fa-lg ml-2 has-text-link">
+              <i class="fa-solid fa-angle-right"></i>
+            </p>
+          </div>
         </div>
-      </div>
-    </div>
-
-    <div class="content m-2" v-if="takingRecords.size !== 0">
-      <div
-        class="media is-flex is-align-items-center is-clickable p-3 m-0"
-        v-for="(takingRecord, takingRecordId) in takingRecords.values"
-        @click="activateTakingRecordModal(takingRecordId)"
-      >
-        <div class="media-left">
-          <figure class="image is-64x64 m-0">
-            <img
-              :src="takingRecord.recorder.profileImageURL"
-              class="is-rounded"
-              v-if="takingRecord.recorder.profileImageURL !== undefined"
-            />
-            <img
-              :src="noProfileImage"
-              class="is-rounded"
-              v-if="takingRecord.recorder.profileImageURL === undefined"
-            />
-          </figure>
+        <!-- TODO: 自動的に読み込まれるように変更する -->
+        <div class="has-text-centered mt-2" v-if="takingRecords.canLoadMore">
+          <button
+            class="button is-small is-ghost"
+            type="button"
+            @click="loadMoreTakingRecords()"
+          >
+            さらに表示する
+          </button>
         </div>
-        <div class="media-content has-text-left has-text-grey-dark">
-          <p class="m-0">
-            <span class="has-text-weight-bold">
-              {{ takingRecord.takenMedicine.medicineName }}
-            </span>
-          </p>
-          <p class="m-0">
-            <span>
-              {{ TakingRecordUtils.toTime(takingRecord.takenAt) }}
-            </span>
-          </p>
-        </div>
-        <div class="media-right">
-          <p class="icon fas fa-lg ml-2 has-text-link">
-            <i class="fa-solid fa-angle-right"></i>
-          </p>
-        </div>
-      </div>
-      <!-- TODO: 自動的に読み込まれるように変更する -->
-      <div class="has-text-centered mt-2" v-if="takingRecords.canLoadMore">
-        <button
-          class="button is-small is-ghost"
-          type="button"
-          @click="loadMoreTakingRecords()"
-        >
-          さらに表示する
-        </button>
       </div>
     </div>
   </div>
