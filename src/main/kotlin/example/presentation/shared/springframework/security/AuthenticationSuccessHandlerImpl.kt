7ebcf68987738a297131971ec48c785015fd412e
hiroe-kk <sntarmi78@gmail.com>
2023-11-17 15:15:51 +0900

    change: 認証に成功した場合に、マイページ画面に遷移するように変更

--- src/main/kotlin/example/presentation/shared/springframework/security/AuthenticationSuccessHandlerImpl.kt
+++ src/main/kotlin/example/presentation/shared/springframework/security/AuthenticationSuccessHandlerImpl.kt
@@ -3,12 +3,8 @@ package example.presentation.shared.springframework.security
 import example.domain.shared.message.*
 import example.presentation.shared.springframework.mvc.*
 import jakarta.servlet.http.*
-import org.springframework.http.server.*
 import org.springframework.security.core.*
 import org.springframework.security.web.authentication.*
-import org.springframework.security.web.savedrequest.*
-import org.springframework.web.util.pattern.*
-import java.net.*
 
 class AuthenticationSuccessHandlerImpl : AuthenticationSuccessHandler {
     /**
@@ -20,24 +16,6 @@ class AuthenticationSuccessHandlerImpl : AuthenticationSuccessHandler {
         val redirectResolver = RedirectResolver(request, response)
         redirectResolver.addFlashAttribute("resultMessage",
                                            ResultMessage.info("ログインに成功しました。"))
-        val redirectPath = getRedirectPath(request, response)
-        redirectResolver.sendRedirect(redirectPath)
-    }
-
-    private fun getRedirectPath(request: HttpServletRequest, response: HttpServletResponse): String {
-        val lastRequestedURI = getPathOfLastRequestedPath(request, response)?.let { excludeErrorPagePaths(it) }
-        return lastRequestedURI ?: "/mypage"
-    }
-
-    private fun getPathOfLastRequestedPath(request: HttpServletRequest, response: HttpServletResponse): String? {
-        val savedRequest = HttpSessionRequestCache().getRequest(request, response)
-        val redirectUrl = savedRequest?.redirectUrl // クライアントによって要求された最後の URI
-        return redirectUrl?.let { URL(it).path } // URL のうちのパス部分のみを抽出する
-    }
-
-    private fun excludeErrorPagePaths(path: String): String? {
-        val pathPattern = PathPatternParser().parse("/error/**")
-        val pathContainer = PathContainer.parsePath(path)
-        return if (pathPattern.matches(pathContainer)) null else path
+        redirectResolver.sendRedirect("/mypage")
     }
 }
