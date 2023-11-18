ba098610c7a7f5396db725743063c34b3116b106
hiroe-kk <sntarmi78@gmail.com>
2023-11-18 17:18:09 +0900

    update: 非公開の場合、アイコンが表示されるように変更

--- src/main/kotlin/example/application/service/medicine/MedicineOverviewDto.kt
+++ src/main/kotlin/example/application/service/medicine/MedicineOverviewDto.kt
@@ -7,14 +7,16 @@ data class MedicineOverviewDto(val medicineId: MedicineId,
                                val medicineName: MedicineName,
                                val dosageAndAdministration: DosageAndAdministration,
                                val medicineImageURL: MedicineImageURL?,
-                               val effects: Effects) {
+                               val effects: Effects,
+                               val isPublic: Boolean) {
     companion object {
         fun from(medicine: Medicine): MedicineOverviewDto {
             return MedicineOverviewDto(medicine.id,
                                        medicine.medicineName,
                                        medicine.dosageAndAdministration,
                                        medicine.medicineImageURL,
-                                       medicine.effects)
+                                       medicine.effects,
+                                       medicine.isPublic)
         }
     }
 }
