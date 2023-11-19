20c5f6ac1ee72f24c7512b93430c48470fb5f43b
hiroe-kk <sntarmi78@gmail.com>
2023-11-19 14:34:18 +0900

    delete: マイページ画面の服用記録一覧からユーザー選択機能を削除

--- frontend/src/main/js/components/medicationrecord/MedicationRecordsByDate.vue
+++ frontend/src/main/js/components/medicationrecord/MedicationRecordsByDate.vue
@@ -10,63 +10,6 @@
         </p>
       </div>
 
-      <div class="content is-inline-block pt-2 m-0" v-if="members.size !== 0">
-        <div class="is-flex is-align-items-center">
-          <div
-            class="is-clickable mx-2"
-            :class="{ opacity: !filter.isUserActive(self.value.accountId) }"
-            @click="toggleUserActive(self.value.accountId)"
-            v-if="self.value !== undefined"
-          >
-            <div class="is-flex is-justify-content-center">
-              <figure class="image is-48x48 m-0">
-                <img
-                  class="is-rounded"
-                  :src="self.value.profileImageURL"
-                  v-if="self.value.profileImageURL !== undefined"
-                />
-                <img
-                  class="is-rounded"
-                  :src="noProfileImage"
-                  v-if="self.value.profileImageURL === undefined"
-                />
-              </figure>
-            </div>
-            <p
-              class="is-size-7 has-text-weight-bold has-text-grey-dark m-0 p-0"
-            >
-              {{ self.value.username }}
-            </p>
-          </div>
-          <div
-            class="is-clickable mx-2"
-            :class="{ opacity: !filter.isUserActive(member.accountId) }"
-            @click="toggleUserActive(member.accountId)"
-            v-for="member in members"
-          >
-            <div class="is-flex is-justify-content-center">
-              <figure class="image is-48x48 m-0">
-                <img
-                  class="is-rounded"
-                  :src="member.profileImageURL"
-                  v-if="member.profileImageURL !== undefined"
-                />
-                <img
-                  class="is-rounded"
-                  :src="noProfileImage"
-                  v-if="member.profileImageURL === undefined"
-                />
-              </figure>
-            </div>
-            <p
-              class="is-size-7 has-text-weight-bold has-text-grey-dark m-0 p-0"
-            >
-              {{ member.username }}
-            </p>
-          </div>
-        </div>
-      </div>
-
       <FilteredMedicationRecords
         ref="filteredDedicationRecords"
         :hasMembers="members.size !== 0"
@@ -81,7 +24,6 @@ import { ref, reactive, onMounted } from 'vue';
 import { Filter } from '@main/js/composables/model/MedicationRecords.js';
 import { HttpRequestClient } from '@main/js/composables/HttpRequestClient.js';
 import FilteredMedicationRecords from '@main/js/components/medicationrecord/FilteredMedicationRecords.vue';
-import noProfileImage from '@main/images/no_profile_image.png';
 import ResultMessage from '@main/js/components/ResultMessage.vue';
 
 const props = defineProps({
@@ -123,23 +65,14 @@ onMounted(async () => {
         );
       });
   }
-
-  filter.start = today();
-  filter.end = today();
+  const date = new Date().toLocaleDateString().slice(0, 10);
+  filter.start = date;
+  filter.end = date;
   filteredDedicationRecords.value.loadMedicationRecords(filter);
 });
-
-function today() {
-  return new Date().toLocaleDateString().slice(0, 10);
-}
 
 function todayWithoutYear() {
   const today = new Date();
   return `${today.getMonth() + 1}月${today.getDate()}日`;
-}
-
-function toggleUserActive(accountId) {
-  filter.toggleUserActive(accountId);
-  filteredDedicationRecords.value.loadMedicationRecords(filter);
 }
 </script>
