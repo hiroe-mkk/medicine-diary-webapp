d7de4fc32ea0ca659cbd2957529c6ba7f384270c
hiroe-kk <sntarmi78@gmail.com>
2023-11-21 20:48:55 +0900

    refactor: UserSearch.vueファイルをsharedgroupパッケージに移動

--- frontend/src/main/js/sharedGroupManagement.js
+++ frontend/src/main/js/sharedGroupManagement.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import UserSearch from '@main/js/components/user/UserSearch.vue';
+import UserSearch from '@main/js/components/sharedgroup/UserSearch.vue';
 import noProfileImage from '@main/images/no_profile_image.png';
 import ConfirmationMessage from '@main/js/components/ConfirmationMessage.vue';
 
