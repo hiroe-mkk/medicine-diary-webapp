178a0afd1921a22987c1118cae8e6fb9b5e6addc
hiroe-kk <sntarmi78@gmail.com>
2023-11-21 20:47:48 +0900

    view: 他の画面に遷移するボタンのデザインを変更

--- frontend/src/main/js/mypage.js
+++ frontend/src/main/js/mypage.js
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
 import noProfileImage from '@main/images/no_profile_image.png';
 import MedicationRecords from '@main/js/components/medicationrecord/MedicationRecordsByDate.vue';
+import medicines from '@main/images/medicines_small.png';
+import calendar from '@main/images/calendar_small.png';
+import writing from '@main/images/writing_small.png';
 
 createApp({
   data() {
     return {
       noProfileImage: noProfileImage,
+      medicines: medicines,
+      writing: writing,
+      calendar: calendar,
     };
   },
   components: {
