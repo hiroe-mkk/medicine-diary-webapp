d2818c3301d9858e6f93c7dffef0457e9d6189d5
hiroe-kk <sntarmi78@gmail.com>
2023-11-25 21:35:05 +0900

    add: 在庫管理を終了するリクエストハンドラを追加

---
+++ src/main/kotlin/example/presentation/controller/api/medicine/InventoryManagementStoppageApiController.kt
@@ -0,0 +1,24 @@
+package example.presentation.controller.api.medicine
+
+import example.application.service.medicine.*
+import example.domain.model.medicine.*
+import example.presentation.shared.usersession.*
+import org.springframework.http.*
+import org.springframework.stereotype.*
+import org.springframework.validation.annotation.*
+import org.springframework.web.bind.annotation.*
+
+@Controller
+@RequestMapping("/api/medicines/{medicineId}/inventory/stop")
+class InventoryManagementStoppageApiController(private val medicineService: MedicineService,
+                                               private val userSessionProvider: UserSessionProvider) {
+    /**
+     * 在庫管理を終了する
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun stopInventoryManagement(@PathVariable medicineId: MedicineId) {
+        medicineService.stopInventoryManagement(medicineId,
+                                                userSessionProvider.getUserSessionOrElseThrow())
+    }
+}
