817701cf23c3378b06165372f1d381e1abbc7993
hiroe-kk <sntarmi78@gmail.com>
2023-12-15 17:02:18 +0900

    refactor: 制約とインデックスを修正

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,27 +1,44 @@
 CREATE TABLE IF NOT EXISTS accounts (
-  account_id VARCHAR(36) PRIMARY KEY,
+  account_id VARCHAR(36) NOT NULL PRIMARY KEY,
   credential_type VARCHAR(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS oauth2_credentials (
-  account_id VARCHAR(36) PRIMARY KEY,
+  account_id VARCHAR(36) NOT NULL PRIMARY KEY,
   id_p VARCHAR(30) NOT NULL,
   subject VARCHAR(255) NOT NULL,
   UNIQUE(id_p, subject),
-  FOREIGN KEY(account_id) REFERENCES accounts(account_id)
+  FOREIGN KEY(account_id) REFERENCES accounts(account_id),
+  INDEX id_p_subject_index (id_p, subject)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS profiles (
-  account_id VARCHAR(36) PRIMARY KEY,
+  account_id VARCHAR(36) NOT NULL PRIMARY KEY,
   username VARCHAR(30) NOT NULL,
   profile_image_url_endpoint VARCHAR(50),
   profile_image_url_path VARCHAR(100),
-  FOREIGN KEY(account_id) REFERENCES accounts(account_id)
+  FOREIGN KEY(account_id) REFERENCES accounts(account_id),
+  INDEX username_index (username)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+
+CREATE TABLE IF NOT EXISTS members (
+  shared_group_id VARCHAR(36) NOT NULL,
+  member VARCHAR(36) NOT NULL,
+  PRIMARY KEY(shared_group_id, member),
+  FOREIGN KEY(member) REFERENCES accounts(account_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE IF NOT EXISTS invitees (
+  shared_group_id VARCHAR(36) NOT NULL,
+  invitee VARCHAR(36) NOT NULL,
+  PRIMARY KEY(shared_group_id, invitee),
+  FOREIGN KEY(invitee) REFERENCES accounts(account_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 
 CREATE TABLE IF NOT EXISTS medicines (
-  medicine_id VARCHAR(36) PRIMARY KEY,
+  medicine_id VARCHAR(36) NOT NULL PRIMARY KEY,
   account_id VARCHAR(36),
   shared_group_id VARCHAR(36),
   medicine_name VARCHAR(30) NOT NULL,
@@ -32,11 +49,13 @@ CREATE TABLE IF NOT EXISTS medicines (
   medicine_image_url_endpoint VARCHAR(50),
   medicine_image_url_path VARCHAR(100),
   is_public BOOLEAN NOT NULL,
-  registered_at DATETIME NOT NULL
+  registered_at DATETIME NOT NULL,
+  FOREIGN KEY(account_id) REFERENCES accounts(account_id),
+  FOREIGN KEY(shared_group_id) REFERENCES members(shared_group_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS inventories (
-  medicine_id VARCHAR(36) PRIMARY KEY,
+  medicine_id VARCHAR(36) NOT NULL PRIMARY KEY,
   remaining_quantity DOUBLE NOT NULL,
   quantity_per_package DOUBLE NOT NULL,
   started_on DATE,
@@ -46,16 +65,16 @@ CREATE TABLE IF NOT EXISTS inventories (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS timing_options (
-  medicine_id VARCHAR(36),
-  ordering INT,
+  medicine_id VARCHAR(36) NOT NULL,
+  ordering INT NOT NULL,
   timing_option VARCHAR(30) NOT NULL,
   PRIMARY KEY(medicine_id, ordering),
   FOREIGN KEY(medicine_id) REFERENCES medicines(medicine_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS effects (
-  medicine_id VARCHAR(36),
-  ordering INT,
+  medicine_id VARCHAR(36) NOT NULL,
+  ordering INT NOT NULL,
   effect VARCHAR(30) NOT NULL,
   PRIMARY KEY(medicine_id, ordering),
   FOREIGN KEY(medicine_id) REFERENCES medicines(medicine_id)
@@ -63,9 +82,9 @@ CREATE TABLE IF NOT EXISTS effects (
 
 
 CREATE TABLE IF NOT EXISTS medication_records (
-  medication_record_id VARCHAR(36) PRIMARY KEY,
-  recorder VARCHAR(36),
-  taken_medicine VARCHAR(36),
+  medication_record_id VARCHAR(36) NOT NULL PRIMARY KEY,
+  recorder VARCHAR(36) NOT NULL,
+  taken_medicine VARCHAR(36) NOT NULL,
   quantity DOUBLE NOT NULL,
   symptom VARCHAR(30) NOT NULL,
   before_medication VARCHAR(15) NOT NULL,
@@ -73,20 +92,6 @@ CREATE TABLE IF NOT EXISTS medication_records (
   note VARCHAR(500) NOT NULL,
   taken_at DATETIME NOT NULL,
   FOREIGN KEY(recorder) REFERENCES accounts(account_id),
-  FOREIGN KEY(taken_medicine) REFERENCES medicines(medicine_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-
-CREATE TABLE IF NOT EXISTS members (
-  shared_group_id VARCHAR(36),
-  member VARCHAR(36),
-  PRIMARY KEY(shared_group_id, member),
-  FOREIGN KEY(member) REFERENCES accounts(account_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE IF NOT EXISTS invitees (
-  shared_group_id VARCHAR(36),
-  invitee VARCHAR(36),
-  PRIMARY KEY(shared_group_id, invitee),
-  FOREIGN KEY(invitee) REFERENCES accounts(account_id)
+  FOREIGN KEY(taken_medicine) REFERENCES medicines(medicine_id),
+  INDEX recorder_taken_medicine_index (recorder, taken_medicine)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
