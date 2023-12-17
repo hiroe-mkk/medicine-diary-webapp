9b89fed7909889a65260194593a35dc84ff5b960
hiroe-kk <sntarmi78@gmail.com>
2023-12-17 22:03:21 +0900

    delete: RDSのロググループを削除

--- terraform/modules/cloudwatch/main.tf
+++ terraform/modules/cloudwatch/main.tf
@@ -2,18 +2,3 @@ resource "aws_cloudwatch_log_group" "springboot" {
   name              = "/ecs/logs/${var.prefix}/springboot"
   retention_in_days = 30
 }
-
-resource "aws_cloudwatch_log_group" "rds-error" {
-  name              = "/rds/logs/${var.prefix}/error"
-  retention_in_days = 30
-}
-
-resource "aws_cloudwatch_log_group" "rds-general" {
-  name              = "/rds/logs/${var.prefix}/general"
-  retention_in_days = 30
-}
-
-resource "aws_cloudwatch_log_group" "rds-slowquery" {
-  name              = "/rds/logs/${var.prefix}/slowquery"
-  retention_in_days = 30
-}
