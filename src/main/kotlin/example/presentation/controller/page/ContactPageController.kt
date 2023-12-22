85151a33b6ff9f37424c62d94438e161bbeb0e34
hiroe-kk <sntarmi78@gmail.com>
2023-12-22 17:13:10 +0900

    add: お問い合わせ内容を確認するリクエストハンドラを追加

--- src/main/kotlin/example/presentation/controller/page/ContactPageController.kt
+++ src/main/kotlin/example/presentation/controller/page/ContactPageController.kt
@@ -1,10 +1,15 @@
 package example.presentation.controller.page
 
 import example.application.service.contact.*
+import example.application.service.medicine.*
 import example.domain.model.contact.*
+import example.domain.shared.message.*
 import org.springframework.stereotype.*
 import org.springframework.ui.*
+import org.springframework.validation.*
+import org.springframework.validation.annotation.*
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.servlet.mvc.support.*
 
 @Controller
 @RequestMapping("/contact")
@@ -16,5 +21,16 @@ class ContactPageController {
     fun displayContactPage(model: Model): String {
         model.addAttribute("form", ContactFormCreationCommand.initialize())
         return "contact/form"
+    }
+
+    /**
+     * お問い合わせ内容を確認する
+     */
+    @PostMapping(params = ["confirm"])
+    fun confirmContactContent(@ModelAttribute("form") @Validated contactFormCreationCommand: ContactFormCreationCommand,
+                         bindingResult: BindingResult): String {
+        if (bindingResult.hasErrors()) return "contact/form"
+
+        return "contact/confirm"
     }
 }
--- src/test/kotlin/example/presentation/controller/page/ContactPageControllerTest.kt
+++ src/test/kotlin/example/presentation/controller/page/ContactPageControllerTest.kt
@@ -5,6 +5,8 @@ import example.testhelper.springframework.autoconfigure.*
 import example.testhelper.springframework.security.*
 import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.*
+import org.springframework.security.test.web.servlet.request.*
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*
 import org.springframework.test.web.servlet.*
 import org.springframework.test.web.servlet.request.*
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
@@ -20,10 +22,52 @@ internal class ContactPageControllerTest(@Autowired private val mockMvc: MockMvc
     @Nested
     inner class DisplayContactPageTest {
         @Test
-        @DisplayName("お問い合わせフォーム画面を表示する")
-        fun displayContactFormPage() {
+        @DisplayName("お問い合わせ画面を表示する")
+        fun displayContactPage() {
             //when:
             val actions = mockMvc.perform(get(PATH))
+
+            //then:
+            actions.andExpect(status().isOk)
+                .andExpect(view().name("contact/form"))
+        }
+    }
+
+    @Nested
+    inner class ConfirmContactContentTest {
+        private val emailAddress = "user@example.co.jp"
+        private val name = "田中太郎"
+        private val content = "お問い合わせ内容"
+
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("お問い合わせ内容確認画面を表示する")
+        fun displayContactContentConfirmationPage() {
+            //when:
+            val actions = mockMvc.perform(post("${PATH}?confirm")
+                                              .with(csrf())
+                                              .param("emailAddress", emailAddress)
+                                              .param("name", name)
+                                              .param("content", content))
+
+            //then:
+            actions.andExpect(status().isOk)
+                .andExpect(view().name("contact/confirm"))
+        }
+
+        @Test
+        @WithMockAuthenticatedAccount
+        @DisplayName("バリデーションエラーが発生した場合、お問い合わせ画面を再表示する")
+        fun validationErrorOccurs_redisplayContactPage() {
+            //given:
+            val invalidEmailAddress = "user @example.co.jp"
+
+            //when:
+            val actions = mockMvc.perform(post("${PATH}?confirm")
+                                              .with(csrf())
+                                              .param("emailAddress", invalidEmailAddress)
+                                              .param("name", name)
+                                              .param("content", content))
 
             //then:
             actions.andExpect(status().isOk)
