776c8a80d2f9f8affc7568dfedbf24f2b9df74e8
hiroe-kk <sntarmi78@gmail.com>
2024-01-06 23:33:36 +0900

    view: 効能がない場合に効能ブロックを表示しないように変更

--- src/main/kotlin/example/domain/model/medicine/Effects.kt
+++ src/main/kotlin/example/domain/model/medicine/Effects.kt
@@ -4,6 +4,8 @@ package example.domain.model.medicine
  * 効能
  */
 data class Effects(val values: List<String>) {
+    fun isEmpty(): Boolean = values.isEmpty()
+
     override fun toString(): String {
         return values.joinToString(transform = { it },
                                    separator = "、",
