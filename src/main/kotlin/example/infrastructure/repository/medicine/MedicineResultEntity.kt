85c658a234def761676599d3731ca5df473ded63
hiroe-kk <sntarmi78@gmail.com>
2024-01-15 14:28:23 +0900

    refactor: MyBatisでList型のプロパティをマッピングする方法を統一

--- src/main/kotlin/example/infrastructure/repository/medicine/MedicineResultEntity.kt
+++ src/main/kotlin/example/infrastructure/repository/medicine/MedicineResultEntity.kt
@@ -16,19 +16,23 @@ class MedicineResultEntity(val medicineId: MedicineId,
                            val isPublic: Boolean,
                            val inventory: Inventory?,
                            val registeredAt: LocalDateTime) {
-    // MyBatis ではコレクションをコンストラクタに渡すことができない
-    val timingOptions: List<OrderedEntity<Timing>> = mutableListOf()
-    val effects: List<OrderedEntity<String>> = mutableListOf()
+    private lateinit var timingOptionsForMapping: List<OrderedEntity<Timing>>
+    val timingOptions: DosageAndAdministration
+        get() = DosageAndAdministration(dose,
+                                        doseUnit,
+                                        timesPerDay,
+                                        OrderedEntitiesConverter.restore(timingOptionsForMapping))
+
+    private lateinit var effectsForMapping: List<OrderedEntity<String>>
+    val effects: Effects
+        get() = Effects(OrderedEntitiesConverter.restore(effectsForMapping))
 
     fun toMedicine(): Medicine {
         return Medicine(medicineId,
                         owner,
                         medicineName,
-                        DosageAndAdministration(dose,
-                                                doseUnit,
-                                                timesPerDay,
-                                                OrderedEntitiesConverter.restore(timingOptions)),
-                        Effects(OrderedEntitiesConverter.restore(effects)),
+                        timingOptions,
+                        effects,
                         precautions,
                         medicineImageURL,
                         isPublic,
--- src/main/kotlin/example/infrastructure/repository/sharedgroup/SharedGroupResultEntity.kt
+++ src/main/kotlin/example/infrastructure/repository/sharedgroup/SharedGroupResultEntity.kt
@@ -4,8 +4,13 @@ import example.domain.model.account.*
 import example.domain.model.sharedgroup.*
 
 class SharedGroupResultEntity(val sharedGroupId: SharedGroupId) {
-    val members: Set<AccountId> = mutableSetOf()
-    val invitees: Set<AccountId> = mutableSetOf()
+    private lateinit var membersForMapping: Set<AccountId>
+    val members: Set<AccountId>
+        get() = membersForMapping
+
+    private lateinit var inviteesForMapping: Set<AccountId>
+    val invitees: Set<AccountId>
+        get() = inviteesForMapping
 
     fun toSharedGroup(): SharedGroup = SharedGroup(sharedGroupId, members, invitees)
 }
