4a9cfa95f7670645e1db36fb1c99674dec5353ef
hiroe-kk <sntarmi78@gmail.com>
2024-09-03 21:22:08 +0900

    refactor: オーバーライドが推奨されているメソッドを追加

--- src/main/kotlin/example/presentation/shared/springframework/security/oauth2/oidc/OidcUserAccount.kt
+++ src/main/kotlin/example/presentation/shared/springframework/security/oauth2/oidc/OidcUserAccount.kt
@@ -2,8 +2,21 @@ package example.presentation.shared.springframework.security.oauth2.oidc
 
 import example.domain.model.account.*
 import example.presentation.shared.springframework.security.oauth2.*
+import org.springframework.security.core.*
 import org.springframework.security.oauth2.core.oidc.user.*
 
 class OidcUserAccount(id: AccountId,
-                      oidcUser: OidcUser) : OAuth2UserAccount(id, oidcUser),
-                                            OidcUser by oidcUser
+                      private val oidcUser: OidcUser) : OAuth2UserAccount(id, oidcUser),
+                                                        OidcUser by oidcUser {
+    override fun getAttributes(): MutableMap<String, Any> {
+        return oidcUser.attributes
+    }
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return oidcUser.authorities
+    }
+
+    override fun getName(): String {
+        return oidcUser.name
+    }
+}
