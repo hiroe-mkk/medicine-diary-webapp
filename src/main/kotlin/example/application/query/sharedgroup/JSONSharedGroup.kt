3d01221107e15b9f33600ada3762129bc20a593d
hiroe-kk <sntarmi78@gmail.com>
2024-09-17 10:20:58 +0900

    refactor: 利用されていないクラスを削除

--- src/main/kotlin/example/application/query/sharedgroup/JSONSharedGroup.kt
+++
@@ -1,9 +0,0 @@
-package example.application.query.sharedgroup
-
-import example.application.query.user.*
-
-class JSONSharedGroup(val sharedGroupId: String) {
-    private lateinit var membersForMapping: Set<JSONUser>
-    val members: Set<JSONUser>
-        get() = membersForMapping
-}
